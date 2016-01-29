4b0f5bfb756dfe8ff5d3c4f29b24aabff72deed0
Sam Kreter <samkreter@gmail.com>
2016-01-28 20:21:20 -0600

    checking changing the head

---
+++ sakfy6_hw1/src/MatrixMultiply.cpp
@@ -0,0 +1,61 @@
+#include "MatrixMultiply.hpp"
+
+
+#include <iostream>
+#include <exception>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <numeric>
+
+scottgs::MatrixMultiply::MatrixMultiply()
+{
+	;
+}
+
+scottgs::MatrixMultiply::~MatrixMultiply()
+{
+	;
+}
+
+
+scottgs::FloatMatrix scottgs::MatrixMultiply::operator()(const scottgs::FloatMatrix& lhs, const scottgs::FloatMatrix& rhs) const
+{
+	// Verify acceptable dimensions
+	if (lhs.size2() != rhs.size1())
+		throw std::logic_error("matrix incompatible lhs.size2() != rhs.size1()");
+
+	scottgs::FloatMatrix result(lhs.size1(),rhs.size2());
+
+	int row = lhs.size1();
+
+	for(int i = 0; i < row; i++){
+		for(int t = 0; t < 1000; t++){
+			std::cout<<"hello"<<std::endl;
+		}
+		for(int j = 0; j < rhs.size2(); j++){
+			int sum = 0;
+			for(int k = 0; k < lhs.size2(); k++){
+
+				sum += lhs(i,k) * rhs(k,j);
+			}
+
+			result(i,j) = sum;
+
+		}
+	}
+
+
+
+
+	return result;
+}
+
+scottgs::FloatMatrix scottgs::MatrixMultiply::multiply(const scottgs::FloatMatrix& lhs, const scottgs::FloatMatrix& rhs) const
+{
+	// Verify acceptable dimensions
+	if (lhs.size2() != rhs.size1())
+		throw std::logic_error("matrix incompatible lhs.size2() != rhs.size1()");
+
+	return boost::numeric::ublas::prod(lhs,rhs);
+}
+
